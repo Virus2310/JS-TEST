1e3b5792e0c9a036337246e6736d937316d0e063
Vlad Prodan <vladprodan6@gmail.com>
2019-04-16 16:37:27 +0300

    Add function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,43 @@
 
-var myBudget = prompt("Ваш бюджет?");
-var nameMagaz = prompt("Название вашего магазина?");
+var myBudget, nameMagaz, price;
+
+function start() {
+    myBudget = prompt("Ваш бюджет?");
+
+    while (isNan(money)|| money == "" || money == null) {
+        myBudget = prompt("Ваш бюджет?");
+    }
+
+    nameMagaz = prompt("Название вашего магазина?").toUpperCase();
+}
+
+
+start();
 
 var mainList = {
     budget : myBudget,
     shopName : nameMagaz,
     shopGoods: [],
     employers: {},
-    open: true
+    open: true,
+    discount: false
 
 }
-
-for(var i = 0;i<3;i++){
-    var a = prompt("Какой тип товаров будем продавать?");
-    
-    if( (typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50) {
-        console.log("Vse Virno");
-        mainList.shopGoods[i] = a; 
-    } else {
-
+function chooseGoods(){
+    for (var i = 0;i<3;i++) {
+        var a = prompt("Какой тип товаров будем продавать?");
+        
+        if( (typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50) {
+            console.log("Vse Virno");
+            mainList.shopGoods[i] = a; 
+        } else {
+            
+        }
     }
 }
+
+chooseGoods();
+
 
 var budgetOneDay= myBudget/30;
 alert("Ваш бюджет на один день " + budgetOneDay);
