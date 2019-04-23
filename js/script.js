92510c82971f0457c4d01ba11a1968d5f7c6119f
Vlad Prodan <vladprodan6@gmail.com>
2019-04-24 00:37:03 +0300

    Add forEach

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var myBudget, nameMagaz, price;
 function start() {
     myBudget = prompt("Ваш бюджет?");
 
-    while (isNaN(myBudget) || myBudget == "" || myBudget == null) {
+    while (isNaN(myBudget) || myBudget === " " || myBudget == null) {
         myBudget = prompt("Ваш бюджет?");
     }
 
@@ -12,63 +12,70 @@ function start() {
 start();
 
 var mainList = {
-    budget: myBudget,
-    shopName: nameMagaz,
-    shopGoods: [],
-    employers: {},
-    open: true,
-    discount: true,
-    shopItems: [],
-    chooseGoods: function chooseGoods() {
-        for (var i = 0; i < 3; i++) {
-            var a = prompt("Какой тип товаров будем продавать?");
+        budget: myBudget,
+        shopName: nameMagaz,
+        shopGoods: [],
+        employers: {},
+        open: true,
+        discount: true,
+        shopItems: [],
+        chooseGoods: function chooseGoods() {
+            for (let i = 0; i < 3; i++) {
+                var a = prompt("Какой тип товаров будем продавать?");
 
-            if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50) {
-                console.log("Vse Virno");
-                mainList.shopGoods[i] = a;
-            } else {
+                if ((typeof(a)) === 'string' && (typeof(a)) === null && a != '' && a.length < 50) {
+                    console.log("Vse Virno");
+                    mainList.shopGoods[i] = a;
+                } else {
 
+                }
             }
-        }
-    },
-    budgetOneDay: function budgetOneDay() {
-        var budgetOneDay = myBudget / 30;
-        alert("Ваш бюджет на один день " + budgetOneDay);
-    },
-    discountSystem: function discountSystem() {
-        price = 1000;
-        while (mainList.discount == true) {
-            disc = price * 80 / 100;
-            alert("Ваша знижка становить " + disc);
-            break;
-        }
-    },
-    addEmployers: function addEmployers() {
-        for (i = 1; i < 4; i++) {
-            let name = prompt("Имя вашего сотрудника");
+        },
 
-            if ((typeof(name)) === 'string' && (typeof(name)) === null && name != '' && a.length < 50) {
-                console.log("Vse Virno");
-                mainList.employers[i] = name;  
-            } else {
-
+        budgetOneDay: function budgetOneDay() {
+            let budgetOneDay = myBudget / 30;
+            alert("Ваш бюджет на один день " + budgetOneDay);
+        },
+        discountSystem: function discountSystem() {
+            price = 1000;
+            while (mainList.discount == true) {
+                disc = price * 80 / 100;
+                alert("Ваша знижка становить " + disc);
+                break;
             }
-        }
-    },
-    chooseShopItems: function chooseShopItems() {
-            var items = prompt("Перерахуйте через кому ваші товари", " ");
+        },
+        addEmployers: function addEmployers() {
+            for (i = 1; i < 4; i++) {
+                let name = prompt("Имя вашего сотрудника");
+
+                if ((typeof(name)) === 'string' && (typeof(name)) === null && name != '' && a.length < 50) {
+                    console.log("Vse Virno");
+                    mainList.employers[i] = name;
+                } else {
+
+                }
+            }
+        },
+        chooseShopItems: function chooseShopItems() {
+            let items = prompt("Перерахуйте через кому ваші товари", "");
+            mainList.shopItems = items;
             mainList.shopItems = items.split(",");
-            mainList.shopItems.push(prompt("Почекайте трохи ", " "));
+            mainList.shopItems.push(prompt("Почекайте трохи ", ""));
             mainList.shopItems.sort();
-    }
-}
+            mainList.shopItems.forEach(function(item, i, arr) {
+                alert("У нас вы можете купить: " + i[1] + ": " + item );
+            });
+        }
+    } // mainList
 
 
 
-mainList.shopGoods.forEach(function(item, i, arr) {
-    alert("У нас вы можете купить: " + );
-});
 
 
+
+mainList.chooseGoods();
+mainList.budgetOneDay();
+mainList.addEmployers();
+mainList.chooseShopItems();
 
 console.log(mainList);
